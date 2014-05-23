bf9625c054ba13ac4b1649015cbf8c72e143d7b1
Shigenori Taya <shigenori.taya@gmail.com>
2014-05-24 02:13:39 +0900

    initial commit 23 Spam Filter

---
+++ 23_Spam_Filter/23_1_spam_filter_core.lisp
@@ -0,0 +1,57 @@
+
+(load "../packages.lisp")
+
+(defpackage :com.gigamonkeys.spam
+  (:use :common-lisp :com.gigamonkeys.pathnames))
+
+(in-package :com.gigamonkeys.spam)
+
+(defun classify (text)
+  (classification (score (extract-features text))))
+
+(defparameter *max-ham-score* .4)
+(defparameter *min-spam-score* .6)
+
+(defun classification (score)
+  (cond
+    ((<= score *max-ham-score*) 'ham)
+    ((>= score *min-spam-score*) 'spam)
+    (t `unsure)))
+
+(defclass word-feature ()
+  ((word
+    :initarg :word
+    :accessor word
+    :initform (error "Must supply :word")
+    :documentation "この特徴を表す単語")
+  (spam-count
+    :initarg :spam-count
+    :accessor spam-count
+    :initform 0
+    :documentation "この特徴が出現したスパムの数")
+  (ham-count
+    :initarg :ham-count
+    :accessor ham-count
+    :initform 0
+    :documentation "この特徴が出現したハムの数")))
+
+(defvar *feature-database* (make-hash-table :test #'equal))
+(defun clear-database ()
+  (setf *feature-database* (make-hash-table :test #'equal)))
+
+(defun intern-feature (word)
+  (or (gethash word *feature-database*)
+      (setf (gethash word *feature-database*)
+            (make-instance 'word-feature :word word))))
+(defun extract-words (text)
+  (delete-duplicates
+    (cl-ppcre:all-matches-as-strings "[a-zA-Z]{3,}" text)
+    :test #'string=))
+(defun extract-features (text)
+  (mapcar #'intern-features (extract-words text)))
+
+(defmethod print-object ((object word-feature) stream)
+  (print-unreadable-object (object stream :type t)
+    (with-slots (word ham-count spam-count) object
+      (format stream "~s :ham ~d :spam ~d" word ham-count spam-count))))
+
