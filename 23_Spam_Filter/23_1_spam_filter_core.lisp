36554158d5acd65e9da0b17d5cccd08f47a135c0
Shigenori Taya <shigenori.taya@gmail.com>
2014-06-05 00:26:38 +0900

    adding packages and cl-ppcre to Chap 23 "Spam Filter"

--- 23_Spam_Filter/23_1_spam_filter_core.lisp
+++ 23_Spam_Filter/23_1_spam_filter_core.lisp
@@ -1,11 +1,39 @@
 
-(load "../packages.lisp")
+; packages
+(in-package :cl-user)
 
+(defpackage :com.gigamonkeys.pathnames
+  (:use :common-lisp)
+  (:export
+   :list-directory
+   :file-exists-p
+   :directory-pathname-p
+   :file-pathname-p
+   :pathname-as-directory
+   :pathname-as-file
+   :walk-directory
+   :directory-p
+   :file-p))
+
+(in-package :com.gigamonkeys.pathnames)
+
+; Chap 23
 (defpackage :com.gigamonkeys.spam
   (:use :common-lisp :com.gigamonkeys.pathnames))
 
 (in-package :com.gigamonkeys.spam)
 
+; required packages
+;(require 'asdf)
+;(require 'asdf-install)
+;(asdf-install:install 'cl-ppcre)
+
+;(load "quicklisp.lisp")
+;(quicklisp-quickstart:install :path ".quicklisp/")
+;(ql:add-to-init-file)
+;(ql:quickload :cl-ppcre)
+
+; main function classify
 (defun classify (text)
   (classification (score (extract-features text))))
 
@@ -18,6 +46,7 @@
     ((>= score *min-spam-score*) 'spam)
     (t `unsure)))
 
+; word-features
 (defclass word-feature ()
   ((word
     :initarg :word
@@ -39,19 +68,28 @@
 (defun clear-database ()
   (setf *feature-database* (make-hash-table :test #'equal)))
 
+;;
 (defun intern-feature (word)
   (or (gethash word *feature-database*)
       (setf (gethash word *feature-database*)
             (make-instance 'word-feature :word word))))
+;;
 (defun extract-words (text)
   (delete-duplicates
     (cl-ppcre:all-matches-as-strings "[a-zA-Z]{3,}" text)
     :test #'string=))
+
+; extract-features
+;; intern-features
+;; extract-words
 (defun extract-features (text)
-  (mapcar #'intern-features (extract-words text)))
+  (mapcar #'intern-feature (extract-words text)))
 
 (defmethod print-object ((object word-feature) stream)
   (print-unreadable-object (object stream :type t)
     (with-slots (word ham-count spam-count) object
       (format stream "~s :ham ~d :spam ~d" word ham-count spam-count))))
 
+; (extract-words "foo bar baz")
+; (extract-words "foo bar baz for bar")
+; (extract-features "foo bar baz foo bar")
